4820d8c37056f4ad4139d99087bbca3973448560
Mitradigitalindonesia <mitradigitalindonesia99@gmail.com>
2025-05-23 01:00:37 +0700

    Update lib.dart

--- lib.dart
+++ lib.dart
@@ -1,26 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AutobotX',
+      debugShowCheckedModeBanner: false,
+      home: const WebViewScreen(),
+    );
+  }
+}
 
-void main() => runApp(MyApp());
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
 
-class MyApp extends StatelessWidget {
-  final String initialUrl = "https://namasituskamu.vercel.app";
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..loadRequest(Uri.parse('https://midix-eight.vercel.app'))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Komunitas App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aplikasi Komunitas'),
-        ),
-        body: SafeArea(
-          child: WebView(
-            initialUrl: initialUrl,
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
-        ),
-      ),
+    return SafeArea(
+      child: WebViewWidget(controller: _controller),
     );
   }
 }
